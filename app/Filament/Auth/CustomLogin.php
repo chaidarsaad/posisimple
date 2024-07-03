f64c370373ebef1deb13f18e5520410283d99cd7
Chaidar Saad <chaidarsaad55@gmail.com>
2024-07-03 11:45:18 +0700

    add login with name or email

---
+++ app/Filament/Auth/CustomLogin.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Auth;
+
+use App\Filament\Pages\Auth\Login;
+use Dotenv\Exception\ValidationException;
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\TextInput;
+
+class CustomLogin extends Login
+{
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getLoginFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        $this->getRememberFormComponent(),
+                    ])
+                    ->statePath('data'),
+            ),
+        ];
+    }
+
+    protected function getLoginFormComponent(): Component
+    {
+        return TextInput::make('login')
+            ->label('Name / Email')
+            ->required()
+            ->autocomplete()
+            ->autofocus()
+            ->extraInputAttributes(['tabindex' => 1]);
+    }
+
+    protected function getCredentialsFromFormData(array $data): array
+    {
+        $login_type = filter_var($data['login'], FILTER_VALIDATE_EMAIL) ? 'email' : 'name';
+
+        return [
+            $login_type => $data['login'],
+            'password'  => $data['password'],
+        ];
+    }
+
+    protected function throwFailureValidationException(): never
+    {
+        throw ValidationException::withMessages([
+            'data.login' => __('filament-panels::pages/auth/login.messages.failed'),
+        ]);
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Auth\CustomLogin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -30,7 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('dashboard')
             ->path('dashboard')
-            ->login(Login::class)
+            ->login(CustomLogin::class)
             ->colors([
                 'primary' => Color::Red,
             ])
